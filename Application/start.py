0da50f2484a0010f840ef5e3dc308a1d9442041b
thomas giroud <thomasgiroud07@gmail.com>
2024-04-12 11:31:02 +0200

    Création du fichier requirements.txt + verification des version des librairies

---
+++ Application/start.py
@@ -0,0 +1,9 @@
+import martypy as marty
+from PyQt6.QtCore import PYQT_VERSION_STR
+import cv2
+import qreader
+
+print("Hello, Marty!")
+print("martypy version:", marty.__version__)
+print("PyQt version:", PYQT_VERSION_STR)
+print("Opencv-python",cv2.__version__)
